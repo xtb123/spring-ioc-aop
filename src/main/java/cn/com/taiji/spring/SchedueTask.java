cffd7f61f7d5ecb715b976d83461e41e6b7ed0ce
xiaotibo <1534320388@qq.com>
2021-01-26 14:36:39 +0800

    one

--- src/main/java/cn/com/taiji/spring/SchedueTask.java
+++ src/main/java/cn/com/taiji/spring/SchedueTask.java
@@ -1,8 +1,14 @@
 package cn.com.taiji.spring;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class SchedueTask {
+public class SchedueTask implements InitializingBean {
 
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("schedueTask init finish ");
+    }
 }
