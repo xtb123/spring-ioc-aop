5ed9d9e8c537119990befaf1f651ef78a4af158b
xiaotibo <1534320388@qq.com>
2021-01-27 16:01:11 +0800

    five

--- src/main/java/cn/com/taiji/spring/LogAspect.java
+++ src/main/java/cn/com/taiji/spring/LogAspect.java
@@ -18,21 +18,35 @@ public class LogAspect {
 
     }
 
-    @Around("pointCut()")
-    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
-//        System.out.println("aspect before");
+   // @Around("pointCut()") //一种方式
+//    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+////        System.out.println("aspect before");
+////        joinPoint.proceed(joinPoint.getArgs());
+////        System.out.println("aspect after");
+//
+//        MethodSignature methodSignature= (MethodSignature) joinPoint.getSignature();
+//        Transaction transaction=methodSignature.getMethod().getAnnotation(Transaction.class);
+//        if(transaction!=null){
+//            System.out.println(transaction.propagate());
+//        }
+//
+//        System.out.println("begin transaction");
 //        joinPoint.proceed(joinPoint.getArgs());
-//        System.out.println("aspect after");
+//        System.out.println("after transaction");
+//    }
 
+    /**
+     * 获取注解还可以这么写，简化方式
+     * @param joinPoint
+     * @param transaction
+     * @throws Throwable
+     */
+    @Around("pointCut() && @annotation(transaction)")
+    public void around(ProceedingJoinPoint joinPoint,Transaction transaction) throws Throwable {
+        System.out.println(transaction.propagate());
 
-        MethodSignature methodSignature= (MethodSignature) joinPoint.getSignature();
-        Transaction transaction=methodSignature.getMethod().getAnnotation(Transaction.class);
-        if(transaction!=null){
-            System.out.println(transaction.propagate());
-        }
-
-        System.out.println("begin transaction");
+        System.out.println("begin1 transaction");
         joinPoint.proceed(joinPoint.getArgs());
-        System.out.println("after transaction");
+        System.out.println("after1 transaction");
     }
 }
